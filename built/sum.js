8019408b128b30ed84ae52bdfe3a84916e7b79ab
Rachael Bermudes <bermudesorama@gmail.com>
2016-08-06 15:01:42 -0700

    added config.json file, but files not recognized

---
+++ built/sum.js
@@ -0,0 +1,6 @@
+var number;
+var otherNumber;
+number = parseInt(prompt('please enter a number.'));
+otherNumber = parseInt(prompt('enter another number'));
+var sum = number + otherNumber;
+alert("The sum of your two numbers is:" + sum);
--- vanilla-sum.js
+++
@@ -1,4 +0,0 @@
-var number = prompt('please enter a number.');
-var otherNumber = prompt('enter another number.');
-var sum = number + otherNumber;
-alert("The sum of your two numbers is: " + sum);
