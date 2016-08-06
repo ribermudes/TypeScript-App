feae0bee8af286706d433f5d52903fb67c43cf44
Rachael Bermudes <bermudesorma@gmail.com>
2016-08-06 13:20:08 -0700

    create typescript files

---
+++ sum.ts
@@ -0,0 +1,7 @@
+var number: number;
+var otherNumber: number;
+
+number = parseInt(prompt('please enter a number.'));
+otherNumber = parseInt(prompt('enter another number'));
+var sum = number + otherNumber;
+alert("The sum of your two numbers is:" + sum);
