c7d164ae31258712ef816a0a6df7a9e2e6827d52
Rachael Bermudes <bermudesorma@gmail.com>
2016-08-07 14:39:57 -0700

    complete typsescript hw exercises

--- sum.js
+++ sum.js
@@ -1,6 +1,7 @@
-var number;
-var otherNumber;
-number = parseInt(prompt('please enter a number.'));
-otherNumber = parseInt(prompt('enter another number'));
-var sum = number + otherNumber;
-alert("The sum of your two numbers is:" + sum);
+var findSum = function (first, second) {
+    var sum = first + second;
+    alert("The sum of your two numbers is: " + sum);
+};
+var number = prompt('please enter a number.');
+var otherNumber = prompt('enter another number.');
+findSum(number, otherNumber);
--- sum.ts
+++ sum.ts
@@ -1,7 +1,8 @@
-var number: number;
-var otherNumber: number;
+var findSum = function(first: number, second: number) {
+  var sum = first + second;
+  alert("The sum of your two numbers is: " + sum);
+}
 
-number = parseInt(prompt('please enter a number.'));
-otherNumber = parseInt(prompt('enter another number'));
-var sum = number + otherNumber;
-alert("The sum of your two numbers is:" + sum);
+var number = parseInt(prompt('please enter a number.'));
+var otherNumber = parseInt(prompt('enter another number.'));
+findSum(number, otherNumber);
